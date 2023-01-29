c41c1a6dffca0e42ca01aa34ede01c1c9c312139
고윤혁 <sksgur3217@gmail.com>
2023-01-30 03:32:01 +0900

    :bulb: [feat] 코드 설명 주석 추가

--- back/socket.js
+++ back/socket.js
@@ -9,14 +9,20 @@ module.exports = (server) => {
       origin: '*',
       credentials: true,
     },
-    allowEIO3: true, // 호환성을 위한 설정 (참고: https://runebook.dev/ko/docs/socketio/client-installation)
-    path: '/socket.io', // path는 클라이언트와 연결할 수 있는 경로를 의미
+    /********************************************************************************************
+     * allowEIO3: 호환성을 위한 설정 (참고: https://runebook.dev/ko/docs/socketio/client-installation)
+     * path: path는 클라이언트와 연결할 수 있는 경로를 의미
+     ********************************************************************************************/
+    allowEIO3: true,
+    path: '/socket.io',
   });
 
   io.on('connection', (socket) => {
     const req = socket.request;
-    // IP 파악
-    // req.headers['x-forwarded-for']는 포워딩 시 ip가 변조되는 부분을 그나마 잡기 위함
+    /************************************************************************
+     * IP 파악
+     * req.headers['x-forwarded-for']는 포워딩 시 ip가 변조되는 부분을 그나마 잡기 위함
+     ************************************************************************/
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(`✔ ${ip} 클라이언트 접속, socket.id : ${socket.id}, req.ip : ${req.ip}`);
 
@@ -30,10 +36,16 @@ module.exports = (server) => {
       console.error(err);
     });
 
-    // 클라이언트로부터의 메세지 (on: 받는다. / emit: 보낸다.)
+    /**********************************************
+     * 클라이언트로부터의 메세지 (on: 받는다. / emit: 보낸다.)
+     **********************************************/
     socket.on('new_message', (msg) => {
       console.log('msg: ', msg);
       console.log('socket.id: ', socket.id);
+      /*******************************************************************
+       * socket.id: 고유의 socket ID를 통해 User를 구분하고 어떠한 작업을 할 수 있다.
+       * 강퇴, 어떤 채팅방에 있는지 확인... 등등
+       *******************************************************************/
       io.emit('new_message', msg);
     });
 
--- front/src/pages/socketIO.js
+++ front/src/pages/socketIO.js
@@ -4,6 +4,16 @@ import io from 'socket.io-client';
 
 const SocketIO = () => {
   const socket = io.connect('http://localhost:8085', {
+    /****************************************************************************************
+     * [transports의 역할]
+     * socketIO 연결 시 브라우저의 네트워크 탭을 확인해보면 socketIO를 지원하지 않는 구버전의 익스플로러를 위해
+     * 연결 시 polling 시도를 먼저 한다.
+     * 이후 websocket 지원이 된다면 websocket으로 연결을 시도 후 연결한다.
+     * transports 설정의 websocket은 'polling 시도 없이 바로 websocket으로 연결 시도를 하겠다.' 라는 뜻이다.
+     * (HTTP 요청 확인 후 websocket도 지원하는지 여부 확인 > websocket으로 프로토콜 전환)
+     * transports 설정이 없다면 polling 시도 시 CORS 에러가 발생한다.
+     * (원인: withCredentials 설정으로 인한 문제라고 추측됨)
+     ****************************************************************************************/
     transports: ['websocket'],
     withCredentials: true,
     path: '/socket.io', // 서버에서 설정한 path와 동일하게 맞춰야 한다.
